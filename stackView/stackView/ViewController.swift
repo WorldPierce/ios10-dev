8708a449664f234117fa5b826a9b5ddbfa110379
worldpierce <bildo316@gmail.com>
2017-08-10 10:54:39 -0400

    stack view project

---
+++ stackView/stackView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stackView
+//
+//  Created by Billy Pierce on 8/10/17.
+//  Copyright © 2017 Billy Pierce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

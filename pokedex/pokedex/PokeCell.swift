86a5ae7a616aebce2682067a24fd9db32cabc469
worldpierce <bildo316@gmail.com>
2017-08-15 13:13:36 -0400

    collection displaying images

---
+++ pokedex/pokedex/PokeCell.swift
@@ -0,0 +1,30 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Billy Pierce on 8/15/17.
+//  Copyright © 2017 Billy Pierce. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    //making rounded corners on ours cells
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        layer.cornerRadius = 5.0
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        nameLbl.text = self.pokemon.name.capitalized
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+    
+}
